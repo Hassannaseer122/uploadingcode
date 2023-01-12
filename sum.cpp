4cfd9d51924f2f278ed463619906201378313930
Hassan Naseer <hassannaseer122@gmail.com>
2023-01-12 13:53:51 +0300

    basic
    basic html code

--- sum.cpp
+++ sum.cpp
@@ -1,10 +1,12 @@
-#include<iostream>
-using namespace std;
-int main()
-{
-  int a,b;
-  a=5;
-  b=5;
-    cout<<a+b;
-  gvbj,lkljuh fcchuo
-}
+<!DOCTYPE html>
+<html>
+<head>
+<title>Page Title</title>
+</head>
+<body>
+
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+</html>
